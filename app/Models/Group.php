710e778344fb4b4ae9e77ad4cf8f391027e0d714
nzbasic <jamescoppard024@gmail.com>
2023-01-17 12:25:12 +1300

    Add genrated doc blocks

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -7,6 +7,31 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Permission\Models\Role;
 
+/**
+ * App\Models\Group
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $name
+ * @property int|null $role_id
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $owners
+ * @property-read int|null $owners_count
+ * @property-read Role|null $role
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $users
+ * @property-read int|null $users_count
+ * @method static \Database\Factories\GroupFactory factory(...$parameters)
+ * @method static Builder|Group newModelQuery()
+ * @method static Builder|Group newQuery()
+ * @method static Builder|Group query()
+ * @method static Builder|Group search(string $search)
+ * @method static Builder|Group whereCreatedAt($value)
+ * @method static Builder|Group whereId($value)
+ * @method static Builder|Group whereName($value)
+ * @method static Builder|Group whereRoleId($value)
+ * @method static Builder|Group whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Group extends Model
 {
     use HasFactory;
